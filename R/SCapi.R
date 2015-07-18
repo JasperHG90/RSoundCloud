306a78e50d67073ab85605b455866cd515e16f23
Jasper Ginn <jasperginn@hotmail.com>
2015-07-19 00:01:31 +0200

    Updates

--- R/SCapi.R
+++ R/SCapi.R
@@ -36,7 +36,7 @@ SCapi <- function(client_id,
     # While limit > 0, make new calls.
     while(limit > 0) {
       # if limit < 200, limit == remainder of modulo
-      if(limit <= 200) {
+      if(limit < 200) {
         tempLim <- limit %% 200
         # Change limit
         res_link <- paste0(gsub("limit=[0-9].*", "", res_link), "limit=", tempLim)
@@ -109,61 +109,67 @@ SCapi <- function(client_id,
   SOUNDCLOUD_LINK != NULL
   '
 
-  # If link, then resolve
-  res_link <- resolve(client_id, soundcloud_link)
-  # Additional information?
-  if(length(...) >= 1) {
-    # Add to list
+  if(!is.null(soundcloud_link)) {
+    # If link, then resolve
+    res_link <- resolve(client_id, soundcloud_link)
+    # Additional information?
     addit <- list(...)
+    # Control
+    if(length(addit) >= 1) {
 
-    "
-    Add information depending on the type of query
-    "
+      "
+      Add information depending on the type of query
+      "
 
-    # USERS
+      # USERS
 
-    if(grepl("users", res_link)) {
-      if(length(addit) > 1) {
-        warning("You can only query one type (e.g. playlists, track etc) at a time, and only the first element will be used")
-      }
-      # Adapt link
-      temp <- unlist(strsplit(res_link, "?", fixed=T))
-      # Paste
-      res_link <- paste0(temp[1], "/", addit[[1]], "?", temp[2], "&limit=", limit)
-      # get json with query
-      jsonDoc <- fromJSON(file = res_link, method='C')
-      # If empty
-      if(length(jsonDoc) == 0) {
-        stop("There are no results for this query. [empty JSON]")
-      }
-      # If error
-      if(length(jsonDoc$errors) != 0) {
-        stop("400 - Bad Request error")
-      }
-      # If limit is larger than 200, use offset (multiple calls). Only do this when the length of the          first query is 200 (otherwise there aren't any more results)
-      if(limit > 200) {
-        if(length(jsonDoc) == 200) {
-          jsonDoc <- paginate(jsonDoc, res_link, limit)
-        } else {
-          warning(paste0("You specified the query limit to be ", limit,
-                         ", but there are only ", length(jsonDoc),
-                         " results. Returning ", length(jsonDoc), " results."))
+      if(grepl("users", res_link)) {
+        if(length(addit) > 1) {
+          warning("You can only query one type (e.g. playlists, track etc) at a time, and only the first element will be used")
         }
+        # Adapt link
+        temp <- unlist(strsplit(res_link, "?", fixed=T))
+        # Paste
+        res_link <- paste0(temp[1], "/", addit[[1]], "?", temp[2], "&limit=", limit)
+        # get json with query
+        jsonDoc <- fromJSON(file = res_link, method='C')
+        # If empty
+        if(length(jsonDoc) == 0) {
+          stop("There are no results for this query. [empty JSON]")
+        }
+        # If error
+        if(length(jsonDoc$errors) != 0) {
+          stop("400 - Bad Request error")
+        }
+        # If limit is larger than 200, use offset (multiple calls). Only do this when the length of the          first query is 200 (otherwise there aren't any more results)
+        if(limit > 200) {
+          if(length(jsonDoc) == 200) {
+            jsonDoc <- paginate(jsonDoc, res_link, limit)
+          } else {
+            warning(paste0("You specified the query limit to be ", limit,
+                           ", but there are only ", length(jsonDoc),
+                           " results. Returning ", length(jsonDoc), " results."))
+          }
+        }
+        # Stop if errors
+        if(length(res$errors) != 0) {
+          stop("Could not fetch result. Please check your input and try again.")
+        }
+        # Return
+        return(jsonDoc)
       }
-      # Stop if errors
-      if(length(res$errors) != 0) {
-        stop("Could not fetch result. Please check your input and try again.")
-      }
-      # Return
-      return(jsonDoc)
     }
+    # (if no additional search terms)
+    # get json with query
+    jsonDoc <- fromJSON(file = res_link, method='C')
+    # Return
+    return(jsonDoc)
   }
-
-
-  # If no info
-
 }
 
+
 res <- SCapi(client_id,
+             searchterm = NULL,
              soundcloud_id = NULL,
-             soundcloud_link = "https://soundcloud.com/the-bugle", "tracks")
+             soundcloud_link = "https://soundcloud.com/deep-chills/dayandnight",
+             limit = 400)
