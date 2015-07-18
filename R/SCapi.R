94f430d522ac428db7199134df59a361dae5a291
Jasper Ginn <jasperginn@hotmail.com>
2015-07-18 15:15:46 +0200

    Working on the main API query function

---
+++ R/SCapi.R
@@ -0,0 +1,15 @@
+# SCapi()
+#
+# Query the soundcloud API
+#
+# Parameters:
+#
+# Returns:
+#
+# Sample use:
+
+
+SCapi <- function(client_id, user_id = NULL, get.id = FALSE) {
+  # Base url
+  base <- "http://api.soundcloud.com/"
+}
