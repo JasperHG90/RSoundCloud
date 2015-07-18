befa02fe76d52efc695f3fe63851c973ea217fc6
Jasper Ginn <jasperginn@hotmail.com>
2015-07-18 17:54:13 +0200

    Added search term and error handling to SCapi function

--- R/SCapi.R
+++ R/SCapi.R
@@ -10,14 +10,56 @@
 
 
 SCapi <- function(client_id,
+                  searchterm = NULL,
                   soundcloud_id = NULL,
                   soundcloud_link = NULL,
                   limit = 50,
                   ... ) {
+
   # Control
-  if(all(is.null(soundcloud_id), is.null(soundcloud_link))) {
+  if(all(is.null(soundcloud_id), is.null(soundcloud_link), is.null(searchterm))) {
     stop("You need to specify either a soundcloud id or a soundcloud link to that item.")
   }
+
+  '
+  Define paginater function
+  '
+
+  paginate <- function(jsonDoc, res_link, limit) {
+    # Decrease limit
+    limit <- limit - 200
+    # Offset value (i.e. where to start)
+    offset <- 200
+    while(limit > 0) {
+      # Construct calls
+      if(limit %% 200 != 0) {
+        tempLim <- limit %% 200
+        # Change limit
+        res_link <- paste0(gsub("limit=[0-9].*", "", res_link), "limit=", tempLim)
+      }
+      # Temp url
+      tempURL <- paste0(res_link, "&offset=", offset)
+      # Call
+      tempCall <- fromJSON(file = tempURL, method='C')
+      # If empty, break
+      if(length(tempCall) == 0) {
+        break
+      }
+      # Add to master
+      jsonDoc <- c(jsonDoc, tempCall)
+      # Increase offset
+      offset <- offset + 200
+      # Decrese limit
+      limit <- limit - 200
+    }
+    # Return
+    return(jsonDoc)
+  }
+
+  '
+  Construct calls
+  '
+
   # If link, then resolve
   res_link <- resolve(client_id, soundcloud_link)
   # Additional information?
@@ -45,33 +87,16 @@ SCapi <- function(client_id,
       if(length(jsonDoc) == 0) {
         stop("There are no results for this query. [empty JSON]")
       }
-      # If limit is larger than 200, use offset (multiple calls)
+      # If error
+      if(length(jsonDoc$errors) != 0) {
+        stop("400 - Bad Request error")
+      }
+      # If limit is larger than 200, use offset (multiple calls). Only do this when the length of the          first query is 200 (otherwise there aren't any more results)
       if(limit > 200) {
-        # Decrease limit
-        limit <- limit - 200
-        # Offset value (i.e. where to start)
-        offset <- 200
-        while(limit > 0) {
-          # Construct calls
-          if(limit %% 200 != 0) {
-            tempLim <- limit %% 200
-            # Change limit
-            res_link <- paste0(gsub("limit=[0-9].*", "", res_link), "limit=", tempLim)
-          }
-          # Temp url
-          tempURL <- paste0(res_link, "&offset=", offset)
-          # Call
-          tempCall <- fromJSON(file = tempURL, method='C')
-          # If empty, break
-          if(length(tempCall) == 0) {
-            break
-          }
-          # Add to master
-          jsonDoc <- c(jsonDoc, tempCall)
-          # Increase offset
-          offset <- offset + 200
-          # Decrese limit
-          limit <- limit - 200
+        if(length(jsonDoc) == 200) {
+          jsonDoc <- paginate(jsonDoc, res_link, limit)
+        } else {
+          warning(paste0("You specified the query limit to be ", limit, ", but there are only ", length(jsonDoc), " results. Returning ", length(jsonDoc), " results."))
         }
       }
       # Stop if errors
