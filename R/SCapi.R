cf96087bd809aac9e423edb9fa93f28b84b5e31d
Jasper Ginn <jasperginn@hotmail.com>
2015-07-18 16:02:34 +0200

    Finished first part of the SC api call.

--- R/SCapi.R
+++ R/SCapi.R
@@ -9,7 +9,48 @@
 # Sample use:
 
 
-SCapi <- function(client_id, user_id = NULL, get.id = FALSE) {
-  # Base url
-  base <- "http://api.soundcloud.com/"
+SCapi <- function(client_id, soundcloud_id = NULL, soundcloud_link = NULL, ... ) {
+  # Control
+  if(all(is.null(soundcloud_id), is.null(soundcloud_link))) {
+    stop("You need to specify either a soundcloud id or a soundcloud link to that item.")
+  }
+  # If link, then resolve
+  res_link <- resolve(client_id, soundcloud_link)
+  # Additional information?
+  if(length(...) >= 1) {
+    # Add to list
+    addit <- list(...)
+
+    "
+    Add information depending on the type of query
+    "
+
+    # USERS
+
+    if(grepl("users", res_link)) {
+      if(length(addit) > 1) {
+        warning("You can only query one type (e.g. playlists, track etc) at a time, and only the first element will be used")
+      }
+      # Adapt link
+      temp <- unlist(strsplit(res_link, "?", fixed=T))
+      # Paste
+      res_link <- paste0(temp[1], "/", addit[[1]], "?", temp[2])
+      # get json with query
+      jsonDoc <- fromJSON(file = res_link, method='C')
+      # Stop if errors
+      if(length(res$errors) != 0) {
+        stop("Could not fetch result. Please check your input and try again.")
+      }
+      # Return
+      return(jsonDoc)
+    }
+  }
+
+
+  # If no info
+
 }
+
+res <- SCapi(client_id,
+             soundcloud_id = NULL,
+             soundcloud_link = "https://soundcloud.com/the-bugle", "tracks")
