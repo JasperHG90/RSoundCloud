0ab81511bf9d2c522d162aac6cd9fdee72769c66
Jasper Ginn <jasperginn@hotmail.com>
2015-07-18 15:24:41 +0200

    Finished resolve function

--- R/resolve.R
+++ R/resolve.R
@@ -3,10 +3,14 @@
 # Resolve the address of a user, playlist, set, comments, groups etc.
 #
 # Parameters:
+#     - client_id: soundcloud client id. See loginDetails() for more information
+#     - soundcloud_url: soundcloud url to resolve
 #
 # Returns:
+#     - character string with the resolved soundcloud url
 #
 # Sample use:
+#     res <- resolve(client_id, "https://soundcloud.com/the-bugle/bonus-bugle-satirist-in-soho-3")
 
 resolve <- function(client_id, soundcloud_url){
   # Base
