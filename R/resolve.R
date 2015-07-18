a17d2e2596e4418a00afe2e5269db8f9d1c5450f
Jasper Ginn <jasperginn@hotmail.com>
2015-07-18 15:22:29 +0200

    Created resolve function

--- R/resolve.R
+++ R/resolve.R
@@ -8,4 +8,21 @@
 #
 # Sample use:
 
-resolve <- function()
+resolve <- function(client_id, soundcloud_url){
+  # Base
+  base <- paste0("http://api.soundcloud.com/resolve?url=", soundcloud_url,
+                 "&client_id=", client_id)
+  # Get redirect
+  curl = getCurlHandle()
+  res <- fromJSON(getURL(base, curl = curl))
+  rm(curl) # release curl
+  # Control if not found
+  if(length(res$errors) != 0) {
+    stop("The url you entered did not yield results. Please check your input and try again.")
+  }
+  # Get redirect url
+  red_url <- res$location
+  # Return
+  return(red_url)
+}
+
