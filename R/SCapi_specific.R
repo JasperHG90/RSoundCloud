d149a02aba7eeee5a456cd403e971dfde78191ef
Jasper Ginn <jasperginn@hotmail.com>
2015-07-19 15:37:20 +0200

    Working on get & filter arguments

--- R/SCapi_specific.R
+++ R/SCapi_specific.R
@@ -1,10 +1,52 @@
 
 SCapi_specific <- function(client_id,
-                         soundcloud_search,
-                         type = c("url", "id", "name"),
-                         query = c("users", "tracks", "playlists", "groups", "comments"),
-                         limit = 50,
-                         ...) {
+                           soundcloud_search,
+                           type = c("url", "id", "name"),
+                           query_type = c("users", "tracks", "playlists", "groups", "comments"),
+                           limit = 50,
+                           get = NULL,
+                           ...) {
+
+  '
+  ++++++++++++++++++++++++++++++++++++++++++++++
+  Check legality of type & query_type parameters
+  ++++++++++++++++++++++++++++++++++++++++++++++
+  '
+
+  # TYPE
+
+  # if type is not chosen
+  if(length(type) > 1) {
+    stop(paste0("Please specify the type of your search term (e.g. url to soundcloud page,",
+                "a soundcloud ID, or a user name.)"))
+  } else{
+    type <- match.arg(type)
+  }
+  # If type is not one of possibilities
+  if(!type %in% c("url", "id", "name")) {
+    stop(paste0("You can only search by soundcloud ID, soundcloud user name, or a url to",
+                "a track, user or playlist etc."))
+  }
+
+  # QUERY_TYPE
+
+  # if query_type is not chosen
+  if(length(query_type) > 1) {
+    stop(paste0("Please choose a query type (e.g. users, playlists etc.)"))
+  } else{
+    type <- match.arg(type)
+  }
+  # If query_type is not one of possibilities
+  if(!query_type %in% c("users", "tracks", "playlists", "groups", "comments")) {
+    stop(paste0("Please choose one of ",
+                "users ", "tracks ", "playlists ", "groups ", "comments ",
+                "as a query type."))
+  }
+
+  # GET
+  if(length(get) > 1) {
+    stop(paste0("'get' can only contain 1 argument."))
+  }
 
   '
   ++++++++++++++++
@@ -65,13 +107,27 @@ SCapi_specific <- function(client_id,
 
   # FUNCTION 4: Construct search urls
 
-  constructURL <- function(client_id, soundcloud_search, type) {
+  constructURL <- function(client_id, soundcloud_search, type, limit, query_type, get) {
     # If url, resolve
     if(type == "url") {
+      # If query_type = user then take any url but remove to generic SC page
+      if(query_type == "users") {
+        # Strip to bare
+        soundcloud_search <- gsub(".*soundcloud.com/", "", soundcloud_search)
+        if(grepl("/", soundcloud_search)) {
+          soundcloud_search <- unlist(strsplit(soundcloud_search, "/"))[1]
+        }
+        soundcloud_search <- paste0("https://soundcloud.com/",
+                                    soundcloud_search)
+        }
+      }
+      # Add get arguments
+      if(!is.null(get)) {
+      soundcloud_search <- paste0(soundcloud_search, "/",
+                                  get)
+      }
       # Resolve
       url <- resolve(client_id, soundcloud_search)
-      # return
-      return(url)
     }
     if(type == "name") {
       #create url
@@ -79,34 +135,73 @@ SCapi_specific <- function(client_id,
                     soundcloud_search)
       # Resolve
       url <- resolve(client_id, url)
-      # Return
-      return(url)
     }
     if(type == "id") {
       # Create url
-
+      url <- paste0("http://api.soundcloud.com/",
+                    query_type, "/",
+                    soundcloud_search,
+                    "?client_id=",
+                    client_id)
     }
+    # Add limit and return
+    url <- paste0(url, "&limit=", limit)
+    return(url)
+  }
+
+  # FUNCTION 5: Paginate through results if limit > 200
+
+  paginate <- function(jsonDoc, res_link, limit) {
+    # Check length of jsonDoc. If < 200, then there are no more results
+    if(length(jsonDoc) < 200) {
+      warning(paste0("You specified the query limit to be ",
+                     limit,
+                     ", but there are only ",
+                     length(jsonDoc),
+                     " results. Returning ",
+                     length(jsonDoc), " results."))
+      return(NULL)
+    }
+    # Decrease limit
+    limit <- limit - 200
+    # Offset value (i.e. where to start)
+    offset <- 200
+    # While limit > 0, make new calls.
+    while(limit > 0) {
+      # if limit < 200, limit == remainder of modulo
+      if(limit < 200) {
+        tempLim <- limit %% 200
+        # Change limit
+        res_link <- paste0(gsub("limit=[0-9].*", "", res_link), "limit=", tempLim)
+      }
+      # Temp url
+      tempURL <- paste0(res_link, "&offset=", offset)
+      # Call
+      tempCall <- fromJSON(file = tempURL, method='C')
+      # If empty, break
+      if(length(tempCall) == 0) {
+        break
+      }
+      # If error, break & warning
+      if(length(tempCall$errors) != 0) {
+        warning("There occurred an error. You may have exceeded your API requests.")
+        break
+      }
+      # Add to master
+      jsonDoc <- c(jsonDoc, tempCall)
+      # Increase offset
+      offset <- offset + 200
+      # Decrese limit
+      limit <- limit - 200
+    }
+    # Return
+    return(jsonDoc)
   }
 
   '
-  Check legality of type parameter
-  '
-
-  # if type is not chosen
-  if(length(type) > 1) {
-    stop(paste0("Please specify the type of your search term (e.g. url to soundcloud page,",
-                "a soundcloud ID, or a user name."))
-  } else{
-    type <- match.arg(type)
-  }
-  # If type is not one of possibilities
-  if(!type %in% c("url", "id", "name")) {
-    stop(paste0("You can only search by soundcloud ID, soundcloud user name, or a url to",
-                "a track, user or playlist etc."))
-  }
-
-  '
+  +++++++++++++++
   Client ID check
+  +++++++++++++++
   '
 
   # Check if client ID is legal
@@ -116,18 +211,26 @@ SCapi_specific <- function(client_id,
   # Check for errors
   error <- errorHandling(res)
   if(!is.null(error)) {
-    stop(error)
+    stop(paste0(error,
+                " (your client ID is not valid. Please check your details and try again.)"))
   }
+
+  '
+  ++++++++++++++++
+  QUERY SOUNDCLOUD
+  ++++++++++++++++
+  '
 
   # Construct url
   # if type is url, then OK
+  page_url <- constructURL(client_id, soundcloud_search, type, limit, query_type, get)
 
-
-
-
-
-
+  return(page_url)
 
 }
 
-SCapiGeneral("3242342", "user", limit = 50)
+SCapi_specific(client_id,
+               "https://soundcloud.com/the-source-2013/sets",
+               type = "url",
+               query_type = "playlists",
+               limit = 50)
